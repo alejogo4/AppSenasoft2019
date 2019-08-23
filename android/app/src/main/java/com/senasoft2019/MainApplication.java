73250a87e2966c64cc15a9ca41a0c77c14450ca8
Alejandro Giraldo Duque <alejo.go4@hotmail.com>
2019-08-22 22:29:00 -0500

    Añadir lectura de QR

--- android/app/src/main/java/com/senasoft2019/MainApplication.java
+++ android/app/src/main/java/com/senasoft2019/MainApplication.java
@@ -3,6 +3,7 @@ package com.senasoft2019;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.wix.RNCameraKit.RNCameraKitPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraKitPackage(),
             new VectorIconsPackage(),
             new SplashScreenReactPackage(),
             new RNGestureHandlerPackage()
--- src/screens/Header/1.js
+++ src/screens/Header/1.js
@@ -1,37 +1,127 @@
 import React, { Component } from "react";
 import {
-  Container,
-  Header,
-  Title,
-  Content,
-  Button,
-  Left,
-  Right,
-  Body,
-  Text
+    Container,
+    Header,
+    Title,
+    Content,
+    Button,
+    Left,
+    Right,
+    Body,
+    Text
 } from "native-base";
+
 import styles from "./styles";
+import { StyleSheet, View, Platform, TouchableOpacity, Linking, PermissionsAndroid } from 'react-native';
+import { CameraKitCameraScreen, } from 'react-native-camera-kit';
 
 class Header1 extends Component {
-  render() {
-    return (
-      <Container style={styles.container}>
-        <Header>
-          <Left />
-          <Body>
-            <Title>Header</Title>
-          </Body>
-          <Right />
-        </Header>
+    constructor() {
 
-        <Content padder>
-          <Button onPress={() => this.props.navigation.goBack()}>
-            <Text>Back</Text>
-          </Button>
-        </Content>
-      </Container>
-    );
-  }
+        super();
+
+        this.state = {
+
+            QR_Code_Value: '',
+
+            Start_Scanner: false,
+
+        };
+    }
+
+    openLink_in_browser = () => {
+
+        Linking.openURL(this.state.QR_Code_Value);
+
+    }
+
+    onQR_Code_Scan_Done = (QR_Code) => {
+
+        this.setState({ QR_Code_Value: QR_Code });
+
+        this.setState({ Start_Scanner: false });
+    }
+
+    open_QR_Code_Scanner=()=> {
+
+        var that = this;
+
+        if (Platform.OS === 'android') {
+            async function requestCameraPermission() {
+                try {
+                    const granted = await PermissionsAndroid.request(
+                        PermissionsAndroid.PERMISSIONS.CAMERA, {
+                            'title': 'Camera App Permission',
+                            'message': 'Camera App needs access to your camera '
+                        }
+                    )
+                    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+
+                        that.setState({ QR_Code_Value: '' });
+                        that.setState({ Start_Scanner: true });
+                    } else {
+                        alert("CAMERA permission denied");
+                    }
+                } catch (err) {
+                    alert("Camera permission err", err);
+                    console.warn(err);
+                }
+            }
+            requestCameraPermission();
+        } else {
+            that.setState({ QR_Code_Value: '' });
+            that.setState({ Start_Scanner: true });
+        }
+    }
+
+    render() {
+        if (!this.state.Start_Scanner) {
+
+            return (
+                <View style={styles.MainContainer}>
+
+                    <Text style={{ fontSize: 22, textAlign: 'center' }}>React Native Scan QR Code Example</Text>
+
+                    <Text style={styles.QR_text}>
+                        {this.state.QR_Code_Value ? 'Dato escaneado: ' + this.state.QR_Code_Value : ''}
+                    </Text>
+
+                    {this.state.QR_Code_Value.includes("http") ?
+                        <TouchableOpacity
+                            onPress={this.openLink_in_browser}
+                            style={styles.button}>
+                            <Text style={{ color: '#FFF', fontSize: 14 }}>Open Link in default Browser</Text>
+                        </TouchableOpacity> : null
+                    }
+
+                    <Button
+                        onPress={this.open_QR_Code_Scanner}
+                        style={styles.button}>
+                        <Text style={{ color: '#FFF', fontSize: 14 }}>
+                            Open QR Scanner
+                        </Text>
+                    </Button>
+
+                </View>
+            );
+        }
+        return (
+            <View style={{ flex: 1 }}>
+
+                <CameraKitCameraScreen
+                    showFrame={true}
+                    scanBarcode={true}
+                    laserColor={'#FF3D00'}
+                    frameColor={'#00C853'}
+                    colorForScannerFrame={'black'}
+                    onReadCode={event =>
+                        this.onQR_Code_Scan_Done(event.nativeEvent.codeStringValue)
+                    }
+                />
+
+            </View>
+        );
+    }
 }
 
 export default Header1;
